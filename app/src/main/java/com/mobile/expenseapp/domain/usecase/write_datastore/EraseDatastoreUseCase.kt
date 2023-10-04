c19dc05fbcef4a14953bb3536b3e03db9adec268
pqtrung <phuquoctrung2003@gmail.com>
2023-10-05 00:36:28 +0700

    domain model

--- app/src/main/java/com/mobile/expenseapp/domain/usecase/write_datastore/EraseDatastoreUseCase.kt
+++ app/src/main/java/com/mobile/expenseapp/domain/usecase/write_datastore/EraseDatastoreUseCase.kt
@@ -1,6 +1,6 @@
-package com.javalon.xpensewhiz.domain.usecase.write_datastore
+package com.mobile.expenseapp.domain.usecase.write_datastore
 
-import com.javalon.xpensewhiz.domain.repository.DatastoreRepository
+import com.mobile.expenseapp.domain.repository.DatastoreRepository
 import javax.inject.Inject
 
 class EraseDatastoreUseCase @Inject constructor(private val datastoreRepository: DatastoreRepository) {
