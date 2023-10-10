77197be61f21d96c057974dda8e869ccdaec7dfe
nam7v3 <nam7v3@gmail.com>
2023-10-10 16:19:38 +0700

    transaction item

--- app/src/main/java/com/mobile/expenseapp/presentation/home_screen/HomeScreen.kt
+++ app/src/main/java/com/mobile/expenseapp/presentation/home_screen/HomeScreen.kt
@@ -1,14 +1,22 @@
 package com.mobile.expenseapp.presentation.home_screen
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.BottomSheetScaffold
 import androidx.compose.material.BottomSheetValue
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.material.rememberBottomSheetScaffoldState
 import androidx.compose.material.rememberBottomSheetState
 import androidx.compose.runtime.Composable
@@ -17,13 +25,19 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.ExperimentalUnitApi
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import com.mobile.expenseapp.presentation.home_screen.components.AddEntryChooser
 import com.mobile.expenseapp.presentation.home_screen.components.Header
+import com.mobile.expenseapp.presentation.home_screen.components.ListPlaceholder
 import com.mobile.expenseapp.presentation.home_screen.components.TabButton
+import com.mobile.expenseapp.presentation.home_screen.components.TransactionItem
+import com.mobile.expenseapp.presentation.navigation.Screen
+import com.mobile.expenseapp.util.spacing
 
 @ExperimentalFoundationApi
 @ExperimentalComposeUiApi
@@ -62,6 +76,97 @@ fun HomeScreen(
 
             // Button tabs
             TabButton()
+
+            // daily expenses
+
+            AnimatedVisibility(visible = currentTabButton == TabButton.TODAY) {
+                dailyTransactions.ifEmpty {
+                    ListPlaceholder()
+                }
+                LazyColumn(
+                    state = lazyListState,
+                    modifier = Modifier
+                        .background(MaterialTheme.colors.background),
+                    contentPadding = PaddingValues(
+                        start = MaterialTheme.spacing.medium,
+                        top = MaterialTheme.spacing.small,
+                        end = MaterialTheme.spacing.medium
+                    )
+                ) {
+                    itemsIndexed(dailyTransactions) { pos, dailyTransaction ->
+                        TransactionItem(
+                            transaction = dailyTransaction,
+                            onItemClick = {
+                                val trxType = dailyTransaction.transactionType
+                                if (trxType == TransactionType.INCOME.title)
+                                    navController.navigate(
+                                        "${Screen.TransactionScreen.route}/0?trxPos=${pos}&trxStatus=${0}"
+                                    )
+                                else
+                                    navController.navigate(
+                                        "${Screen.TransactionScreen.route}/1?trxPos=${pos}&trxStatus=${0}"
+                                    )
+                            }
+                        )
+                    }
+                }
+            }
+
+            // monthly expenses
+            AnimatedVisibility(visible = currentTabButton == TabButton.MONTH) {
+                monthlyTransactions.ifEmpty {
+                    ListPlaceholder()
+                }
+                LazyColumn(
+                    state = lazyListState,
+                    modifier = Modifier
+                        .background(MaterialTheme.colors.background),
+                    contentPadding = PaddingValues(
+                        start = MaterialTheme.spacing.medium,
+                        top = MaterialTheme.spacing.small,
+                        end = MaterialTheme.spacing.medium
+                    )
+                ) {
+                    monthlyTransactions.forEach { (date, monthlyTransaction) ->
+                        stickyHeader {
+                            Row(
+                                horizontalArrangement = Arrangement.SpaceBetween,
+                                verticalAlignment = Alignment.CenterVertically,
+                                modifier = Modifier
+                                    .padding(
+                                        horizontal = MaterialTheme.spacing.medium,
+                                        vertical = MaterialTheme.spacing.small
+                                    ),
+                            ) {
+                                Text(
+                                    text = date,
+                                    style = MaterialTheme.typography.body2,
+                                    color = MaterialTheme.colors.onBackground,
+                                    fontWeight = FontWeight.Bold,
+                                    textAlign = TextAlign.Start
+                                )
+                            }
+                        }
+
+                        itemsIndexed(monthlyTransaction) { pos, transaction ->
+                            TransactionItem(
+                                transaction = transaction,
+                                onItemClick = {
+                                    val trxType = transaction.transactionType
+                                    if (trxType == TransactionType.INCOME.title)
+                                        navController.navigate(
+                                            "${Screen.TransactionScreen.route}/0?trxKey=${date}&trxPos=${pos}&trxStatus=${1}"
+                                        )
+                                    else
+                                        navController.navigate(
+                                            "${Screen.TransactionScreen.route}/1?trxKey=${date}&trxPos=${pos}&trxStatus=${1}"
+                                        )
+                                }
+                            )
+                        }
+                    }
+                }
+            }
         }
     }
 
