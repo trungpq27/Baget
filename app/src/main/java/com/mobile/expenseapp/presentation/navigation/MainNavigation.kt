e07bd229d09f62c2c92a41d21b9ba4552fdf3ae5
nam7v3 <nam7v3@gmail.com>
2023-10-10 16:15:13 +0700

    transaction screen

--- app/src/main/java/com/mobile/expenseapp/presentation/navigation/MainNavigation.kt
+++ app/src/main/java/com/mobile/expenseapp/presentation/navigation/MainNavigation.kt
@@ -12,6 +12,7 @@ import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.mobile.expenseapp.presentation.home_screen.HomeScreen
+import com.mobile.expenseapp.presentation.home_screen.TransactionScreen
 import com.mobile.expenseapp.presentation.setting_screen.SettingScreen
 import com.mobile.expenseapp.presentation.welcome_screen.CurrencyScreen
 import com.mobile.expenseapp.presentation.welcome_screen.WelcomeScreen
@@ -45,6 +46,35 @@ fun MainNavigation(
         }
         composable(route = Screen.HomeScreen.route) {
             HomeScreen(navController = navController)
+        }
+        composable(route = "${Screen.TransactionScreen.route}/{tag}?trxKey={trxKey}&trxPos={trxPos}&trxStatus={trxStatus}",
+            arguments = listOf(
+                navArgument("tag") {
+                    type = NavType.IntType
+                    defaultValue = 0
+                },
+                navArgument("trxKey") {
+                    type = NavType.StringType
+                    defaultValue = null
+                    nullable = true
+                },
+                navArgument("trxPos") {
+                    type = NavType.IntType
+                    defaultValue = -1
+                },
+                navArgument("trxStatus") {
+                    type = NavType.IntType
+                    defaultValue = -1
+                }
+            )
+        ) { entry ->
+            TransactionScreen(
+                navController = navController,
+                transactionTag = entry.arguments?.getInt("tag"),
+                transactionDate = entry.arguments?.getString("trxKey"),
+                transactionPos = entry.arguments?.getInt("trxPos"),
+                transactionStatus = entry.arguments?.getInt("trxStatus")
+            )
         }
         composable(route = Screen.SettingScreen.route) {
             SettingScreen(navController = navController)
