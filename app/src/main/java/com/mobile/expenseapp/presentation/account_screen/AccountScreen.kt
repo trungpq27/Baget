a96f319f56da55fed56261424505ee49930b623e
lataonhehe <21020395@vnu.edu.vn>
2023-10-10 16:53:57 +0700

    Account-screen init

---
+++ app/src/main/java/com/mobile/expenseapp/presentation/account_screen/AccountScreen.kt
@@ -0,0 +1,52 @@
+package com.mobile.expenseapp.presentation.account_screen
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavHostController
+import com.mobile.expenseapp.presentation.navigation.Screen
+import com.mobile.expenseapp.util.spacing
+
+@ExperimentalMaterialApi
+@Composable
+fun AccountScreen(
+    navController: NavHostController,
+    accountViewModel: AccountViewModel = hiltViewModel()
+) {
+    val accounts by accountViewModel.allAccounts.collectAsState()
+    val currency by accountViewModel.selectedCurrencyCode.collectAsState()
+    Surface(
+        color = MaterialTheme.colors.background
+    ) {
+        LazyColumn {
+            item {
+                Text(
+                    text = "Accounts",
+                    style = MaterialTheme.typography.h4.copy(fontWeight = FontWeight.W700),
+                    color = MaterialTheme.colors.onSurface,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(
+                            start = MaterialTheme.spacing.medium,
+                            end = MaterialTheme.spacing.medium,
+                            top = MaterialTheme.spacing.small
+                        ),
+                    textAlign = TextAlign.Start
+                )
+
+            }
+        }
+    }
+}
