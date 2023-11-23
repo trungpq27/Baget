2297eb83f8987f72f4d0dd2ef2859b1525ca608c
lataonhehe <21020395@vnu.edu.vn>
2023-11-23 23:34:12 +0700

    fix database

--- app/src/main/java/com/mobile/expenseapp/data/local/entity/ScheduleDto.kt
+++ app/src/main/java/com/mobile/expenseapp/data/local/entity/ScheduleDto.kt
@@ -2,14 +2,28 @@ package com.mobile.expenseapp.data.local.entity
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
 import com.mobile.expenseapp.domain.model.Schedule
+import com.mobile.expenseapp.domain.model.Transaction
 import java.util.Date
 import java.util.concurrent.TimeUnit
 
-@Entity(tableName = "schedule_table")
+@Entity(
+    tableName = "schedule_table",
+    foreignKeys = [
+        ForeignKey(
+            entity = TransactionDto::class,
+            parentColumns = ["timestamp"],
+            childColumns = ["transactionTimestamp"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ]
+)
 data class ScheduleDto(
-    @ColumnInfo(name = "transaction")
-    val transactionDto: TransactionDto,
+    @PrimaryKey
+    @ColumnInfo(name = "transactionTimestamp")
+    val transactionDto: Date,
     @ColumnInfo(name = "time_schedule")
     val timeSchedule: Long,
     @ColumnInfo(name = "time_unit")
@@ -18,7 +32,15 @@ data class ScheduleDto(
     val lastTimeAdded: Date,
 ) {
     fun toSchedule() = Schedule(
-        transaction = transactionDto.toTransaction(),
+        transaction = Transaction(
+            date = transactionDto,
+            dateOfEntry = "", // Provide appropriate default values or modify the constructor accordingly
+            amount = 0.0,
+            account = "",
+            category = "",
+            transactionType = "",
+            title = ""
+        ),
         timeSchedule = timeSchedule,
         timeUnit = TimeUnit.valueOf(timeUnit),
         lastTimeAdded = lastTimeAdded
